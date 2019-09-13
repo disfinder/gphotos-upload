81e2b0009e53b53dfd1bdfd99c2aca1d7ccc5205
Dis Finder <3998461+disfinder@users.noreply.github.com>
2019-09-13 15:27:13 -0400

    autoformat

--- upload.py
+++ upload.py
@@ -7,6 +7,7 @@ import argparse
 import logging
 from tqdm import tqdm
 
+
 class TqdmHandler(logging.StreamHandler):
     def __init__(self):
         logging.StreamHandler.__init__(self)
@@ -15,16 +16,17 @@ class TqdmHandler(logging.StreamHandler):
         msg = self.format(record)
         tqdm.write(msg, file=sys.stderr)
 
+
 def parse_args(arg_input=None):
     parser = argparse.ArgumentParser(description='Upload photos to Google Photos.')
     parser.add_argument('--auth ', metavar='auth_file', dest='auth_file',
-                    help='file for reading/storing user authentication tokens')
+                        help='file for reading/storing user authentication tokens')
     parser.add_argument('--album', metavar='album_name', dest='album_name',
-                    help='name of photo album to create (if it doesn\'t exist). Any uploaded photos will be added to this album.')
+                        help='name of photo album to create (if it doesn\'t exist). Any uploaded photos will be added to this album.')
     parser.add_argument('--log', metavar='log_file', dest='log_file',
-                    help='name of output file for log messages')
-    parser.add_argument('photos', metavar='photo',type=str, nargs='*',
-                    help='filename of a photo to upload')
+                        help='name of output file for log messages')
+    parser.add_argument('photos', metavar='photo', type=str, nargs='*',
+                        help='filename of a photo to upload')
     return parser.parse_args(arg_input)
 
 
@@ -41,10 +43,10 @@ def auth(scopes):
 
     return credentials
 
-def get_authorized_session(auth_token_file):
 
-    scopes=['https://www.googleapis.com/auth/photoslibrary',
-            'https://www.googleapis.com/auth/photoslibrary.sharing']
+def get_authorized_session(auth_token_file):
+    scopes = ['https://www.googleapis.com/auth/photoslibrary',
+              'https://www.googleapis.com/auth/photoslibrary.sharing']
 
     cred = None
 
@@ -55,7 +57,6 @@ def get_authorized_session(auth_token_file):
             logging.debug("Error opening auth token file - {0}".format(err))
         except ValueError:
             logging.debug("Error loading auth tokens - Incorrect format")
-
 
     if not cred:
         cred = auth(scopes)
@@ -72,7 +73,6 @@ def get_authorized_session(auth_token_file):
 
 
 def save_cred(cred, auth_file):
-
     cred_dict = {
         'token': cred.token,
         'refresh_token': cred.refresh_token,
@@ -86,12 +86,12 @@ def save_cred(cred, auth_file):
     with open(auth_file, 'w') as f:
         print(json.dumps(cred_dict), file=f)
 
+
 # Generator to loop through all albums
 
 def getAlbums(session, appCreatedOnly=False):
-
     params = {
-            'excludeNonAppCreatedData': appCreatedOnly
+        'excludeNonAppCreatedData': appCreatedOnly
     }
 
     while True:
@@ -113,9 +113,9 @@ def getAlbums(session, appCreatedOnly=False):
         else:
             return
 
-def create_or_retrieve_album(session, album_title):
 
-# Find albums created by this app to see if one matches album_title
+def create_or_retrieve_album(session, album_title):
+    # Find albums created by this app to see if one matches album_title
 
     for a in getAlbums(session, True):
         if a["title"].lower() == album_title.lower():
@@ -123,10 +123,10 @@ def create_or_retrieve_album(session, album_title):
             logging.info("Uploading into EXISTING photo album -- \'{0}\'".format(album_title))
             return album_id
 
-# No matches, create new album
+    # No matches, create new album
 
-    create_album_body = json.dumps({"album":{"title": album_title}})
-    #print(create_album_body)
+    create_album_body = json.dumps({"album": {"title": album_title}})
+    # print(create_album_body)
     resp = session.post('https://photoslibrary.googleapis.com/v1/albums', create_album_body).json()
 
     logging.debug("Server response: {}".format(resp))
@@ -138,8 +138,8 @@ def create_or_retrieve_album(session, album_title):
         logging.error("Could not find or create photo album '\{0}\'. Server Response: {1}".format(album_title, resp))
         return None
 
-def upload_photos(session, photo_file_list, album_name):
 
+def upload_photos(session, photo_file_list, album_name):
     album_id = create_or_retrieve_album(session, album_name) if album_name else None
 
     # interrupt upload if an upload was requested but could not be created
@@ -151,65 +151,73 @@ def upload_photos(session, photo_file_list, album_name):
 
     for photo_file_name in photo_file_list:
 
-            try:
-                photo_file = open(photo_file_name, mode='rb')
-                photo_bytes = photo_file.read()
-            except OSError as err:
-                logging.error("Could not read file \'{0}\' -- {1}".format(photo_file_name, err))
-                continue
+        try:
+            photo_file = open(photo_file_name, mode='rb')
+            photo_bytes = photo_file.read()
+        except OSError as err:
+            logging.error("Could not read file \'{0}\' -- {1}".format(photo_file_name, err))
+            continue
 
-            session.headers["X-Goog-Upload-File-Name"] = os.path.basename(photo_file_name)
+        session.headers["X-Goog-Upload-File-Name"] = os.path.basename(photo_file_name)
 
-            logging.info("Uploading photo -- \'{}\'".format(photo_file_name))
+        logging.info("Uploading photo -- \'{}\'".format(photo_file_name))
 
-            upload_token = session.post('https://photoslibrary.googleapis.com/v1/uploads', photo_bytes)
+        upload_token = session.post('https://photoslibrary.googleapis.com/v1/uploads', photo_bytes)
 
-            if (upload_token.status_code == 200) and (upload_token.content):
+        if (upload_token.status_code == 200) and (upload_token.content):
 
-                create_body = json.dumps({"albumId":album_id, "newMediaItems":[{"description":"","simpleMediaItem":{"uploadToken":upload_token.content.decode()}}]}, indent=4)
+            create_body = json.dumps({"albumId": album_id, "newMediaItems": [
+                {"description": "", "simpleMediaItem": {"uploadToken": upload_token.content.decode()}}]}, indent=4)
 
-                resp = session.post('https://photoslibrary.googleapis.com/v1/mediaItems:batchCreate', create_body).json()
+            resp = session.post('https://photoslibrary.googleapis.com/v1/mediaItems:batchCreate', create_body).json()
 
-                logging.debug("Server response: {}".format(resp))
+            logging.debug("Server response: {}".format(resp))
 
-                if "newMediaItemResults" in resp:
-                    status = resp["newMediaItemResults"][0]["status"]
-                    if status.get("code") and (status.get("code") > 0):
-                        logging.error("Could not add \'{0}\' to library -- {1}".format(os.path.basename(photo_file_name), status["message"]))
-                    else:
-                        logging.info("Added \'{}\' to library and album \'{}\' ".format(os.path.basename(photo_file_name), album_name))
+            if "newMediaItemResults" in resp:
+                status = resp["newMediaItemResults"][0]["status"]
+                if status.get("code") and (status.get("code") > 0):
+                    logging.error("Could not add \'{0}\' to library -- {1}".format(os.path.basename(photo_file_name),
+                                                                                   status["message"]))
                 else:
-                    logging.error("Could not add \'{0}\' to library. Server Response -- {1}".format(os.path.basename(photo_file_name), resp))
-
+                    logging.info("Added \'{}\' to library and album \'{}\' ".format(os.path.basename(photo_file_name),
+                                                                                    album_name))
             else:
-                logging.error("Could not upload \'{0}\'. Server Response - {1}".format(os.path.basename(photo_file_name), upload_token))
+                logging.error(
+                    "Could not add \'{0}\' to library. Server Response -- {1}".format(os.path.basename(photo_file_name),
+                                                                                      resp))
+
+        else:
+            logging.error("Could not upload \'{0}\'. Server Response - {1}".format(os.path.basename(photo_file_name),
+                                                                                   upload_token))
 
     try:
-        del(session.headers["Content-type"])
-        del(session.headers["X-Goog-Upload-Protocol"])
-        del(session.headers["X-Goog-Upload-File-Name"])
+        del (session.headers["Content-type"])
+        del (session.headers["X-Goog-Upload-Protocol"])
+        del (session.headers["X-Goog-Upload-File-Name"])
     except KeyError:
         pass
 
-def main():
 
+def main():
     args = parse_args()
 
     logging.basicConfig(format='%(asctime)s %(module)s.%(funcName)s:%(levelname)s:%(message)s',
-                    datefmt='%m/%d/%Y %I_%M_%S %p',
-                    filename=args.log_file,
-                    level=logging.INFO)
+                        datefmt='%m/%d/%Y %I_%M_%S %p',
+                        filename=args.log_file,
+                        level=logging.INFO)
 
     session = get_authorized_session(args.auth_file)
 
     upload_photos(session, args.photos, args.album_name)
 
-    # As a quick status check, dump the albums and their key attributes
+    # As a quick status check, dump the aglbums and their key attributes
 
-    print("{:<50} | {:>8} | {} ".format("PHOTO ALBUM","# PHOTOS", "IS WRITEABLE?"))
+    print("{:<50} | {:>8} | {} ".format("PHOTO ALBUM", "# PHOTOS", "IS WRITEABLE?"))
 
     for a in getAlbums(session):
-        print("{:<50} | {:>8} | {} ".format(a["title"],a.get("mediaItemsCount", "0"), str(a.get("isWriteable", False))))
+        print(
+            "{:<50} | {:>8} | {} ".format(a["title"], a.get("mediaItemsCount", "0"), str(a.get("isWriteable", False))))
+
 
 if __name__ == '__main__':
-  main()
+    main()
